a3d9faf879895e367ad17ddb313782b207823212
HolkerDev <mykola.penkovskyi@gmail.com>
2022-07-05 19:57:20 +0200

    init project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{self, get, routes, response::status::Created, Response};
+use lambda_web::{is_running_on_lambda, launch_rocket_on_lambda, LambdaError};
+use rocket::http::Status;
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[rocket::main]
+async fn main() -> Result<(), LambdaError> {
+    let rocket = rocket::build().mount("/", routes![hello]);
+    if is_running_on_lambda() {
+        // Launch on AWS Lambda
+        launch_rocket_on_lambda(rocket).await?;
+    } else {
+        // Launch local server
+        let _ = rocket.launch().await?;
+    }
+    Ok(())
 }
